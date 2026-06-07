0d2a272c2849ce15379f0bd0251e347ba4f3d60e
Adriana_Sullca_Colque <asullca12@gmail.com>
2026-06-07 02:08:10 -0400

    chore: commit inicial en main

---
+++ src/main/java/com/example/tallergitavanzado_p4sis414/TallerGitAvanzadoP4Sis414Application.java
@@ -0,0 +1,13 @@
+package com.example.tallergitavanzado_p4sis414;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerGitAvanzadoP4Sis414Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerGitAvanzadoP4Sis414Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tallergitavanzado_p4sis414/TallerGitAvanzadoP4Sis414ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tallergitavanzado_p4sis414;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerGitAvanzadoP4Sis414ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
